afb02fed20c350ba70b48097b3a919897977bdc1
Rawfur Rahman <rawfurrahman4@gmail.com.com>
2025-07-23 22:59:04 +0600

    Fixed Evidence Hub Button with fully functional User Management Module

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -15,7 +15,7 @@
                     Projects
                 </a>
                 
-                <a href="#" class="text-gray-500 group flex items-center px-2 py-2 text-sm font-medium rounded-md cursor-not-allowed" aria-disabled="true" onclick="return false;">
+                <a href="{{ route('evidence.show', ['project' => 1]) }}" class="{{ request()->routeIs('evidence.show') ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' }} group flex items-center px-2 py-2 text-sm font-medium rounded-md">
                     <i class="fas fa-folder-open mr-3 fa-fw"></i>
                     Evidence Hub
                 </a>
